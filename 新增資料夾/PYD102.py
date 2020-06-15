9d77231400de1b2ddfab926ff8eef71330a072fe
yu-wei <gggggaaaaasssss12345@gmail.com>
2020-06-15 11:23:40 +0800

    add PYD102
    add PYD102

---
+++ 新增資料夾/PYD102.py
@@ -0,0 +1,18 @@
+# TODO
+a=eval(input())
+b=eval(input())
+c=eval(input())
+d=eval(input())
+# 靠右對齊
+# TODO
+print("|%7.2f %7.2f|" % (a,b))
+print("|%7.2f %7.2f|" % (c,d))
+print("|%-7.2f %-7.2f|" % (a,b))
+print("|%-7.2f %-7.2f|" % (c,d))
+# 靠左對齊
+# TODO 
+'''23.12
+395.3
+100.4617
+564.329
+'''
